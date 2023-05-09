046c9cba2acde8ad692ad438460bb17f057aa517
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-05-09 14:05:12 +0800

    chore: handle messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import "./App.css"
 
 import { socket } from "./socket"
@@ -6,11 +6,17 @@ import { socket } from "./socket"
 function App() {
   const [_, setIsConnected] = useState(socket.connected)
 
+  const onMessages = useCallback((msg: any) => {
+    console.log(msg)
+  }, [])
+
   useEffect(() => {
     socket.on("connect", () => setIsConnected(true))
+    socket.on("message", onMessages)
 
     return () => {
       socket.off("connect", () => console.log("disconnect"))
+      socket.off("message", onMessages)
     }
   }, [])
 
